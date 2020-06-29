8c4318713829fd3a678357e52eadcee05d5e2f6f
Aureuser <1355087842@qq.com>
2020-06-29 23:16:31 +0800

    在直接中加入用空间换时间的小方法

--- 84. 柱状图中最大的矩形/栈方法.py
+++ 84. 柱状图中最大的矩形/栈方法.py
@@ -2,11 +2,15 @@ class Solution:
     def largestRectangleArea(self, heights):
         FILO = []
         area = 0
+        # area列表中初始化一个0， 防止heights为空时area为空
+        # area = [0, ]
         for i in range(len(heights)):
             while FILO and heights[i] < heights[FILO[-1]]:
                 index = FILO.pop()
                 if FILO:
                     area = max(area, heights[index] * (i-FILO[-1]-1))
+                    # 也可以将area做成列表，面积汇总之后最后做max，用空间换时间
+                    # area.append(heights[index] * (i - FILO[-1] - 1))
                 else:
                     area = max(area, heights[index] * i)
             FILO.append(i)
