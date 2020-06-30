0674e4d66ee48bd6daa2bc362f5b2e3b091e4fc8
Aureuser <1355087842@qq.com>
2020-06-30 23:23:52 +0800

    粗糙的动态规划实现最大矩阵

--- 85. 最大矩形/动态规划.py
+++ 85. 最大矩形/动态规划.py
@@ -11,19 +11,22 @@ class Solution:
         for row in matrix:
             max_left_width = []
             for i in row:
-                max_left_width.append(max_left_width[-1] + 1) if i == '1' and max_left_width \
-                    else max_left_width.append(0)
+                if max_left_width:
+                    max_left_width.append(max_left_width[-1] + 1) if i == '1' and max_left_width \
+                        else max_left_width.append(0)
+                elif i == '1':
+                    max_left_width.append(1)
+                else:
+                    max_left_width.append(0)
             max_left_width_list.append(max_left_width)
-        for i in range(len(matrix[0])):
-            width = 0
-            height = 0
-            for j in range(len(matrix)):
-                height += 1
-                if width < max_left_width_list[j][i]:
-                    height = 1
-                width = max_left_width_list[j][i]
-                area = max(area, width * height)
-
+        # print(max_left_width_list)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if max_left_width_list[i][j] == 0: continue
+                width = len(matrix[0])
+                for k in range(i, len(matrix)):
+                    width = min(width, max_left_width_list[k][j])
+                    area = max(width * (k - i + 1), area)
         return area
 
 if __name__ == '__main__':
