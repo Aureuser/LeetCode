2cd2d3b28d57566679ce193f259698b63f2d5197
Aureuser <1355087842@qq.com>
2020-07-01 13:07:36 +0800

    添加栈法实现求最大矩形的代码 该代码结合84.柱状图最大矩阵的栈方法实现

--- 85. 最大矩形/栈方法.py
+++ 85. 最大矩形/栈方法.py
@@ -4,5 +4,55 @@
 # @File : 栈方法.py
 
 class Solution:
+    '''
+    该栈方法是基于84.柱状图中最大的矩阵的站方法实现的
+    之前我们用过动态规划算法求出每一列元素的最大宽，通过列方向宽度矩阵类似于柱状图
+    柱状图的高度就是矩阵中宽度数值，
+    '''
+    def largestRectangleArea(self, heights):
+        height_list = list()
+        area = 0
+        for i in range(len(heights)):
+            while height_list and heights[i] < heights[height_list[-1]]:
+                top = height_list.pop()
+                if height_list:
+                    area = max(area, heights[top] * (i - height_list[-1] - 1))
+                else:
+                    area = max(area, heights[top] * i)
+            height_list.append(i)
+        while height_list:
+            top = height_list.pop()
+            if height_list:
+                area = max(area, heights[top] * (len(heights) - height_list[-1] - 1))
+            else:
+                area = max(area, heights[top] * len(heights))
+        return area
+
+
     def maximalRectangle(self, matrix):
-        pass
+        if not matrix:
+            return 0
+        area = 0
+        widths = [0 for _ in range(len(matrix[0]))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                widths[j] = widths[j] + 1 if matrix[i][j] == '1' else  0
+            area = max(self.largestRectangleArea(widths), area)
+        return area
+
+if __name__ == '__main__':
+    s = Solution()
+    area = s.maximalRectangle([
+        ["1","0","1","0","0"],
+        ["1","0","1","1","1"],
+        ["1","1","1","1","1"],
+        ["1","0","0","1","0"]])
+    # 6
+    print(area)
+    area = s.maximalRectangle([
+        ["0","0","1","0"],
+        ["0","1","1","1"],
+        ["0","1","1","0"],
+        ["1","0","1","0"]])
+    # 4
+    print(area)
