efab10dc2d6ccf1fdb1ea19ff09be0bd19df5e50
Aureuser <1355087842@qq.com>
2020-07-06 21:55:31 +0800

    栈方法实现统计全1子矩阵

--- 5454. 统计全 1 子矩形/栈方法.py
+++ 5454. 统计全 1 子矩形/栈方法.py
@@ -1,9 +1,10 @@
 class Solution:
     '''
     本方法借用85.最大矩阵的栈方法
-    但还是有些许不同，在85中栈内是递增的，但是允许包含相同的高度，比如[1,2,3,4,4,5]
-    但是在笨方法中，栈内是严格递增的，即不允许包含相同高度，比如[1,2,3,4,5,6]
-    目前本方法还存在缺陷。
+    但还是有些许不同，在85中,出栈时计算出栈元素左右两端的下标差再乘以自身高度，因为栈内递增，比该元素大的元素已经提前出栈了
+    而左边的第一个是除该元素之外该元素左边第一大的数（也可能与该元素登高），同理如果该元素右边存在比该元素小的数，那么该元素已经出栈，
+    所以它右边是第一个比它小的数，所以该元素的最大宽度是右元素下标与左元素下标之间的宽度。
+    5454中，出栈的目的是求元素左边能组成的最大矩阵，所以不再是右元素下标-做元素下标，而是我也说不清楚
     '''
     def numSubmat(self, mat):
         num = 0
@@ -11,40 +12,19 @@ class Solution:
             return 0
         height = [0 for _ in mat[0]]
         for i in range(len(mat)):
+            widths = list()
+            sum = 0
             for j in range(len(mat[0])):
                 if mat[i][j] == 1: height[j] +=  1
                 else: height[j] = 0
-            num += self.largestRectangleArea(height)
+                while widths and height[widths[-1]] >= height[j]:
+                    idx = widths.pop()
+                    width = idx - (widths[-1] if widths else -1)
+                    sum -= width * (height[idx] - height[j])
+                widths.append(j)
+                sum += height[j]
+                num += sum
         return num
-
-    def largestRectangleArea(self, heights):
-        height_list = list()
-        num = 0
-        for i in range(len(heights)):
-            while height_list and heights[i] <= heights[height_list[-1]]:
-                top = height_list.pop()
-                if height_list:
-                    num += self.get_sun(heights[top], (i - height_list[-1] - 1))
-                else:
-                    num += self.get_sun(heights[top], i)
-            height_list.append(i)
-        while height_list:
-            top = height_list.pop()
-            if height_list:
-                num += self.get_sun(heights[top], (len(heights) - height_list[-1] - 1))
-            else:
-                num += self.get_sun(heights[top], len(heights))
-        # print(num)
-        return num
-
-    def get_sun(self, width, height):
-        '''
-        计算一个大矩阵包含多少个小矩阵(包括自身)
-        :param width: 矩阵宽度
-        :param height: 矩阵高度
-        :return: 子矩阵个数
-        '''
-        return sum([i * j for i in range(1, width + 1) for j in range(1, height + 1)])
 
 if __name__ == '__main__':
     s = Solution()
